b463bc04fbadb08ce31c559df7262a97798e6366
MuFeng086 <mufeng086@gmail.com>
2023-09-23 13:08:53 +0000

    Update main to output generated at d34ce47

--- assets/pageview-458cc093.js
+++
@@ -1,1 +0,0 @@
-import{L as e}from"./app-69a3c14f.js";const r=()=>e({serverURL:"https://waline-1-j6551192.deta.app/"});export{r as updatePageview};
---
+++ assets/pageview-aea05c77.js
@@ -0,0 +1,1 @@
+import{L as e}from"./app-9daae805.js";const r=()=>e({serverURL:"https://waline-1-j6551192.deta.app/"});export{r as updatePageview};
